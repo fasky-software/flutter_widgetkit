8dfe8aba7fa7d50e6d59ab7c207d55c6adb4e63b
Thomas Leiter <thomas.leiter@outlook.at>
2020-10-29 18:39:56 +0100

    Updated Readme

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -51,30 +51,14 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('iOS Widget Showcase'),
         ),
         body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Enter a text',
+              'Enter a text 🙏🏻',
             ),
             Container(
               margin: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
@@ -84,7 +68,7 @@ class _MyAppState extends State<MyApp> {
             RaisedButton(onPressed: () {
               WidgetKit.setItem('widgetData', jsonEncode(FlutterWidgetData(textController.text)), 'group.com.fasky');
               WidgetKit.reloadAllTimelines();
-            }, child: Text("Send to Widget"))
+            }, child: Text('Update Widget 🥳'))
           ],
         ),
         ),
